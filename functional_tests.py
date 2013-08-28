c7447b178fc6e7bb0df21fa8d2f9de0e92f221a2
Colin Carroll <colinc@spiceworks.com>
2013-08-27 19:46:25 -0500

    Functional tests now use unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,33 @@
 """ Functional test suite
 """
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+  """ Tells story of a new visitor to the site
+  """
 
-assert 'Django' in browser.title
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+    self.browser.implicitly_wait(3)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_upload_and_view_result(self):
+    """Navigate to uploads, enter results
+    """
+    # Mitchell visits the homepage of a results app
+    self.browser.get('http://localhost:5000')
+
+    #Seeing the page title lets him know he's in the right place
+    self.assertIn('BugBug', self.browser.title)
+    self.fail("Finish the test!")
+
+    #He clicks a button to upload new results and is taken to the uploads
+    #page.  He enters a url pointing to results, and clicks the upload
+    #button.  After a series of confirmation screens, this takes him to
+    #the parsed results page.
+
+if __name__ == '__main__':
+  unittest.main()
