f74eaec3ed0b3daa2db86767b96b3d315ae60cb9
Colin Carroll <colinc@spiceworks.com>
2013-09-14 16:35:58 -0500

    Added views for courses

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,22 @@
-""" Create your views here.
+""" Course views
 """
-from django.http import HttpResponse
+from django.shortcuts import render
+from courses.models import Course
 
 def homepage(request):
-  """Home page for teams
+  """Home page for courses
   """
-  return HttpResponse('<html><title>BugBug Courses</title></html>')
+  courses = Course.objects.all()
+  return render(request,
+      'courses/homepage.html',
+      {'courses' : courses})
+
+def results(request, course_pk):
+  """Home page for a course
+  """
+  course = Course.objects.get(pk=course_pk)
+  the_meets = course.meet_set.all().order_by('date')
+  return render(request,
+      'courses/results.html',
+      {'course' : course,
+      'meets': the_meets})
