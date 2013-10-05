7d7aa29852030ea0251e0c4141f1eb891c1daab5
Colin Carroll <colinc@spiceworks.com>
2013-10-05 13:29:24 -0500

    Manually editing migrations

--- runners/migrations/0001_initial.py
+++ runners/migrations/0001_initial.py
@@ -13,26 +13,15 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('first_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('last_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('team', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['teams.Team'])),
             ('class_year', self.gf('django.db.models.fields.IntegerField')()),
         ))
         db.send_create_signal('runners', ['Runner'])
-
-        # Adding M2M table for field teams on 'Runner'
-        m2m_table_name = db.shorten_name('runners_runner_teams')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('runner', models.ForeignKey(orm['runners.runner'], null=False)),
-            ('team', models.ForeignKey(orm['teams.team'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['runner_id', 'team_id'])
 
 
     def backwards(self, orm):
         # Deleting model 'Runner'
         db.delete_table('runners_runner')
-
-        # Removing M2M table for field teams on 'Runner'
-        db.delete_table(db.shorten_name('runners_runner_teams'))
 
 
     models = {
@@ -42,7 +31,7 @@ class Migration(SchemaMigration):
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'teams': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['teams.Team']"})
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['teams.Team']"})
         },
         'teams.team': {
             'Meta': {'object_name': 'Team'},
