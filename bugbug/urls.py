18fcb30e00d2a0b6c84d8d3ffdaa2a51d78788df
Colin Carroll <colinc@spiceworks.com>
2013-10-06 18:08:25 -0500

    Trying to serve static files

--- bugbug/urls.py
+++ bugbug/urls.py
@@ -1,6 +1,7 @@
 """URL routing for project
 """
 from django.conf.urls import patterns, url, include
+from bugbug import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,11 +17,12 @@ urlpatterns = patterns('',
     url(r'^courses/(\d+)', 'courses.views.results', name='courses_meets'),
     url(r'^courses/', 'courses.views.homepage', name='courses_home'),
     url(r'^predictions/',
-      'predictions.views.homepage',
-      name='predictions_home'),
+        'predictions.views.homepage',
+        name='predictions_home'),
     url(r'^runners/(\d+)', 'runners.views.results', name='runners_results'),
     url(r'^runners/search/', 'runners.views.search', name='runner_search'),
-    url(r'^runners/search/',include('haystack.urls'))
+    url(r'^runners/search/',include('haystack.urls')),
+    url(r'^static/(.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
     # url(r'^bugbug/', include('bugbug.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
